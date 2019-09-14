dc455f9627d0115df7651b39d33868e77abf42e6
YasirAlharbi <46800088+YasirAlharbi@users.noreply.github.com>
2019-09-14 16:40:04 +0300

    Add files via upload

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+    "dev": {
+      "username": "udagramyasirdev",
+      "password": "udagramyasirdev",
+      "database": "udagramyasirdev",
+      "host": "udagramyasirdev.cowxeopexn6q.us-east-2.rds.amazonaws.com",
+      "dialect": "postgres",
+      "aws_reigion": "us-east-2",
+      "aws_profile": "default",
+      "aws_media_bucket": "udagram-yasir-dev"
+    },
+    "prod": {
+      "username": "",
+      "password": "",
+      "database": "udagram_prod",
+      "host": "",
+      "dialect": "postgres"
+    }
+  }
---
+++ www/config/config.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "dev": {
+        "username": "udagramyasirdev",
+        "password": "udagramyasirdev",
+        "database": "udagramyasirdev",
+        "host": "udagramyasirdev.cowxeopexn6q.us-east-2.rds.amazonaws.com",
+        "dialect": "postgres",
+        "aws_reigion": "us-east-2",
+        "aws_profile": "default",
+        "aws_media_bucket": "udagram-yasir-dev"
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    }
+};
+//# sourceMappingURL=config.js.map
